af7c57f65fb7bce4101a8616e7c747ab9f4199c4
LLJ2004 <2208227328@qq.com>
2025-07-07 20:40:28 +0800

    处理通用的文件上传和下载请求，包括单个文件上传、多个文件上传、通用文件下载以及本地资源下载。

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/common/CommonController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/common/CommonController.java
@@ -23,7 +23,7 @@ import com.ruoyi.framework.config.ServerConfig;
 
 /**
  * 通用请求处理
- * 
+ *
  * @author ruoyi
  */
 @RestController
@@ -39,7 +39,7 @@ public class CommonController
 
     /**
      * 通用下载请求
-     * 
+     *
      * @param fileName 文件名称
      * @param delete 是否删除
      */
